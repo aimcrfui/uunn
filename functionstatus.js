f22aecb9c496e73845a9962fbb8e1a4c6b5bb5e7
aimcrfui <43334837+aimcrfui@users.noreply.github.com>
2022-05-06 20:41:10 +0800

    Update functionstatus.js

--- functionstatus.js
+++ functionstatus.js
@@ -15,7 +15,7 @@ $done({
     title:"𝗦𝗨𝗥𝗚𝗘 已运行"+startTime,
     content:"Mitm:"+icon_status(mitm_status.enabled)+"  Rewrite:"+icon_status(rewrite_status.enabled)+"  Scripting:"+icon_status(scripting_status.enabled),
     icon: "globe",
-   "icon-color":"#965454"
+   "icon-color":"#96A48B"
     
 });
 })();
