66b3f091cf90e65a12a5586cac3e09151ba03cc7
aimcrfui <43334837+aimcrfui@users.noreply.github.com>
2022-07-10 19:58:18 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,15 +1,13 @@
-function setHost(p, host) {
-    if (['vmess', 'vless'].includes(p.type)) {
-      p.servername = host
-      if (p.network === 'ws') {
-        $.lodash_set(p, 'ws-opts.headers.Host', host)
-      } else if (p.network === 'h2') {
-        $.lodash_set(p, 'h2-opts.host', [host])
-      } else if (p.network === 'http') {
-        $.lodash_set(p, 'http-opts.headers.Host', [host])
-      } else if (p.network) {
-        $.lodash_set(p, `${p.network}-opts.headers.Host`, [host])
-      }
-    }
-    return p
-  }
+function operator(proxies) {
+    const { host } = $arguments;
+    proxies.forEach(p => {
+        if (p.type === 'vmess' && p.network === 'ws') {
+            p["ws-opts"] = p["ws-opts"] || {};
+            p["ws-opts"]["headers"] = p["ws-opts"]["headers"] || {};
+            p["ws-opts"]["headers"]["Host"] = host;
+        } else (p.network === 'http'){
+            p["http-opts"]["headers"]["Host"] = host;
+        }
+    });
+    return proxies;
+}
